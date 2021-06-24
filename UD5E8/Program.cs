6f6162bde35a23f1e166171632a06e99bd943364
Martina <martinaescorial9@gmail.com>
2021-06-24 17:19:14 +0200

    Add project files.

---
+++ UD5E8/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace UD5E8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intArray = { 1, 2, 3, 4 };
+
+            Array.ForEach(intArray, Console.WriteLine);
+
+        }
+
+        
+
+        
+        
+    }
+}
